df2819d9b0c7550cd8f88e8e5e16e37db9cd7ed4
Vinícius Bohrer dos Santos <vinicius.bohrer@voalle.com.br>
2023-01-24 22:10:00 -0300

    teste alterando código-fonte 3

--- src/main/java/com/rafael/fazumbem/email/EmailService.java
+++ src/main/java/com/rafael/fazumbem/email/EmailService.java
@@ -26,7 +26,7 @@ public class EmailService{
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "utf-8");
             mimeMessageHelper.setText(email, true);
             mimeMessageHelper.setTo(to);
-            mimeMessageHelper.setSubject("Acesso - Faz um Bem 2");
+            mimeMessageHelper.setSubject("Acesso - Faz um Bem 3");
             mimeMessageHelper.setFrom("fazumbem@inf.ufsm.br");
             javaMailSender.send(mimeMessage);
         } catch(MessagingException e) {
